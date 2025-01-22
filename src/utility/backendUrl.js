e30e8500109b91ba5256234cf6c4c9fff7e0e3e8
artemmuk999 <artemmuknorenko@gmail.com>
2025-01-22 03:03:22 -0700

    update backend url

--- src/utility/backendUrl.js
+++ src/utility/backendUrl.js
@@ -1,4 +1,4 @@
 // const backendURL = 'http://localhost:3000';
-const backendURL = 'https://credit-card-backend.vercel.app/';
+const backendURL = 'https://credit-card-backend.vercel.app';
 
 export default backendURL;
