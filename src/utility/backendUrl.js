42545596d6bd63da6c3c5b0a402e4a39f0ee122e
VictorDS <victorking90111@gmail.com>
2025-01-21 20:11:29 -0700

    update backend url

--- src/utility/backendUrl.js
+++ src/utility/backendUrl.js
@@ -1,4 +1,4 @@
-const backendURL = 'http://localhost:3000';
 // const backendURL = 'http://localhost:3000';
+const backendURL = 'https://creditcardbackend.vercel.app/';
 
 export default backendURL;
